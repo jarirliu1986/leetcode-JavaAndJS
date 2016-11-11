1d178a122b3c925a5363af138328b6e5663d98e5
jarirliu1986 <289374247@qq.com>
2016-11-10 16:22:12 -0800

    changes

--- Java/src/GuessGame.java
+++ Java/src/GuessGame.java
@@ -3,13 +3,13 @@
    @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
       int guess(int num); */
 
-public class Solution extends GuessGame {
+public class GuessGame {
     public int guessNumber(int n) {
         int s = 1, e = n;
         int mid = 0;
         while(s <= e){
             mid = s+(e-s)/2;
-            int guess = guess(mid);
+            int guess = guessNumber(mid);
             if(guess == 1){
                 s = mid+1;
             }else if(guess == -1){
