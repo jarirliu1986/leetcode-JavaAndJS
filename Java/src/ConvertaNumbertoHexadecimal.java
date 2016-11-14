1b753ac8b0760fcc6c54c7df05c628d41b74f17d
jarirliu1986 <289374247@qq.com>
2016-11-13 23:18:12 -0800

    change directory

---
+++ Java/src/ConvertaNumbertoHexadecimal.java
@@ -0,0 +1,16 @@
+
+public class ConvertaNumbertoHexadecimal {
+    public String toHex(int num) {
+    	
+        
+    	return "";
+    }
+    public static void main(String[] args){
+    	int n=12;
+    	char c = 'A';
+    	String s = "hello";
+    	s = "hi"+s  ;
+    	
+    	System.out.println(s);
+    }
+}
