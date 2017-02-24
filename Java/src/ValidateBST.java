8c3bf7c24e16cd5d770b17d3cefc7fb8b5c8d73a
jarirliu1986 <289374247@qq.com>
2017-02-24 11:38:16 -0800

    +1 for tree

--- Java/src/ValidateBST.java
+++ Java/src/ValidateBST.java
@@ -1,4 +1,16 @@
 
 public class ValidateBST {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        
+    }
 
+    public boolean helper(TreeNode root, long start, long end){
+    	if(root == null) return true;
+    	if(root.val <= start || root.val >= end)
+    		return false;
+    	
+    	return helper(root.right, root.val, end) && helper(root.left, start, root.val);
+    }
+    
 }
