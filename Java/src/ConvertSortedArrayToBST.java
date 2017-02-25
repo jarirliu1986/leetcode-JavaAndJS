5f5b5b0e4f40787175b7738da83e1fcd0ea93331
jarirliu1986 <289374247@qq.com>
2017-02-24 16:57:34 -0800

    2 tree

---
+++ Java/src/ConvertSortedArrayToBST.java
@@ -0,0 +1,16 @@
+
+public class ConvertSortedArrayToBST {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null|| nums.length == 0) return null;
+        return helper(nums, 0, nums.length-1);
+    }
+    
+    public TreeNode helper(int[] nums, int start, int end){
+        if(start > end) return null;
+        int mid = (start+end)/2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = helper(nums, start, mid-1);
+        node.right = helper(nums, mid+1, end);
+        return node;
+    }
+}
