0ecf14cf1d533a13aad276a3c6cd34b7461fb9c4
jarirliu1986 <289374247@qq.com>
2017-03-01 23:40:43 -0800

    make changed

--- Java/src/PathSumII.java
+++ Java/src/PathSumII.java
@@ -1,4 +1,25 @@
+import java.util.*;
 
 public class PathSumII {
-
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+    	List<List<Integer>> list = new LinkedList<>();
+        List<Integer> alist = new LinkedList<Integer>();
+        helper(list, alist, root, sum);
+        return list;
+    }
+    
+    public void helper(List<List<Integer>> list, List<Integer> alist, TreeNode root, int sum){
+    	if(root == null) return;
+    	
+    	alist.add(root.val);
+    	if(root.left == null && root.right == null && root.val == sum){
+    		list .add(new LinkedList<Integer>(alist));
+    		alist.remove(alist.size()-1);
+    		return;
+    	}
+    	
+    	helper(list, alist, root.left, sum-root.val);
+    	helper(list, alist, root.right, sum-root.val);
+    	alist.remove(alist.size()-1);
+    }
 }
